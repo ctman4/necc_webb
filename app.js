b5c7272c84853434cadcd91bb1e8e6ed26aa4ad3
ctman4 <charlietirrell@gmail.com>
2021-06-24 18:57:31 -0400

    added ejs

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 var router = express.Router();
 
+app.set('view engine', 'ejs');
+
 app.use('/', routes);
 
-router.get('/', function(req, res, next) {
-    res.render('index', {title: 'Express'});
+app.get('/', (req, res) => {
+    res.render(__dirname + '/views/index.ejs');
 });
 
 app.listen(5000, () => {
