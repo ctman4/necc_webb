fb2bffe02cbb783d2119ab8ca2224ecb854bf7f7
ctman4 <charlietirrell@gmail.com>
2021-06-25 11:28:54 -0400

    set views

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.set('views', './views');
 app.use(express.urlencoded({extended: true}));
 
 //public folders
