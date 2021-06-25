6d826fe3c4875c5c0d849ec354107900cbf943c9
ctman4 <charlietirrell@gmail.com>
2021-06-25 17:50:57 -0400

    added express-openid-connect and dotenv and auth0

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const router = require('./router');
 const connect = require('./db');
+require('dotenv').config();
 const app = express();
 
 app.set('view engine', 'ejs');
